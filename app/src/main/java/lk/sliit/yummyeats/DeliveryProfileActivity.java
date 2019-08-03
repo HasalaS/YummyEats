e8a101d3a57d9193595125acc6555e4bca228c32
shaminandrado <shaminandrado@gmail.com>
2019-08-04 01:09:56 +0530

    Update some functions in customer, deliver, restaurant profiles.

--- app/src/main/java/lk/sliit/yummyeats/DeliveryProfileActivity.java
+++ app/src/main/java/lk/sliit/yummyeats/DeliveryProfileActivity.java
@@ -1,7 +1,9 @@
 package lk.sliit.yummyeats;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DeliveryProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class DeliveryProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_profile);
+        getSupportActionBar().hide();
+    }
+
+    public void showDialogDeliver(View view){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(DeliveryProfileActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_invalid_password, null);
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/lk/sliit/yummyeats/RestaurantProfileActivity.java
+++ app/src/main/java/lk/sliit/yummyeats/RestaurantProfileActivity.java
@@ -1,9 +1,11 @@
 package lk.sliit.yummyeats;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class RestaurantProfileActivity extends AppCompatActivity {
@@ -22,5 +24,13 @@ public class RestaurantProfileActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
 
+    }
+
+    public void showDialogRestaurant(View view){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(RestaurantProfileActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_invalid_password, null);
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
     }
 }
