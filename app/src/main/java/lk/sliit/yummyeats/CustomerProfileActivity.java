f7eb2e8f41c92c3b9d7b6cb311f44a7e2fb54de8
ThamalDilanka <thamaldilanke@gmail.com>
2019-09-15 13:11:46 +0530

    Update dialog box completed.

--- app/src/main/java/lk/sliit/yummyeats/CustomerProfileActivity.java
+++ app/src/main/java/lk/sliit/yummyeats/CustomerProfileActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import lk.sliit.yummyeats.Model.SessionUser;
 
 public class CustomerProfileActivity extends AppCompatActivity {
 
-    TextView etName;
+    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +28,49 @@ public class CustomerProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_profile);
         getSupportActionBar().hide();
 
-        etName = findViewById(R.id.cus_profile_name);
-        etName.setText(SessionUser.customer.getName());
-
+        tvName = findViewById(R.id.tv_cus_profile_card_name_value);
+        tvName.setText(SessionUser.customer.getName());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-
     }
 
     public void showDialogCustomer(View view){
+
+        TextView tvKey = null;
+        TextView tvValue = null;
+
+        ViewGroup card = (ViewGroup) view;
+        tvKey = (TextView) card.getChildAt(0);
+        tvValue = (TextView) card.getChildAt(1);
+
+        String key = tvKey.getText().toString();
+        String value = tvValue.getText().toString();
+
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(CustomerProfileActivity.this);
         View mView = getLayoutInflater().inflate(R.layout.dialog_update, null);
+
+        ViewGroup vg = (ViewGroup)mView;
+        ViewGroup viewGroup = (ViewGroup) vg.getChildAt(0);
+
+        TextView updateTvKey = (TextView) viewGroup.getChildAt(0);
+        EditText updateEtValue = (EditText) viewGroup.getChildAt(1);
+        Button updateBtn = (Button) viewGroup.getChildAt(2);
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //write update code here
+                Toast.makeText(CustomerProfileActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        updateTvKey.setText(key);
+        updateEtValue.setText(value);
+
         mBuilder.setView(mView);
         AlertDialog dialog = mBuilder.create();
         dialog.show();
